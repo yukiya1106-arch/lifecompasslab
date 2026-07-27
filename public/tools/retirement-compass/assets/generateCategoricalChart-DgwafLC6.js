b17af5c57c71a3e69d31a50f62c7a272c774ca96
yukiya1106-arch <yukiya1106@gmail.com>
2026-07-27 11:06:20 +0900

    Use 38-year default service period

--- public/tools/retirement-compass/assets/generateCategoricalChart-DgwafLC6.js
+++ public/tools/retirement-compass/assets/generateCategoricalChart-DgwafLC6.js
@@ -1,4 +1,4 @@
-import{c as qm,b as Hn,g as ue,r as N,a as T}from"./index-DnX6QJVw.js";/**
+import{c as qm,b as Hn,g as ue,r as N,a as T}from"./index-lBMZ18GD.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- public/tools/retirement-compass/assets/trending-up-C9t5zPvf.js
+++ public/tools/retirement-compass/assets/trending-up-C9t5zPvf.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-DnX6QJVw.js";/**
+import{c as e}from"./index-lBMZ18GD.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
